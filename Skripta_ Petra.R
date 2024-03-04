42e4380cc33e819b9e2aa1b5d2cd3210553ce42f
Vukinator <35816915+Vukinator@users.noreply.github.com>
2024-03-04 06:27:04 +0100

    Update Skripta_ Petra.R

--- Skripta_ Petra.R
+++ Skripta_ Petra.R
@@ -19,7 +19,7 @@ if(a<b){
   print(paste(a, "je veći od", paste(b)))
   print("Uvjet (a<b) nije zadovoljen")
 } # "9 je veći od 7"
-  # "Uvjet (a<b) nije zadovoljen"
+# "Uvjet (a<b) nije zadovoljen"
 
 x= -3
 if(x<0){
@@ -122,11 +122,11 @@ while(x<=10){
   print(paste("Iznos je", x))
   x= x+1
 } # "Iznos je 1"
-  # "Iznos je 2"
-  # "Iznos je 3"
-  # "Iznos je 4"
-  # "Iznos je 5"
-  # "Iznos je 6" itd. sve do 10
+# "Iznos je 2"
+# "Iznos je 3"
+# "Iznos je 4"
+# "Iznos je 5"
+# "Iznos je 6" itd. sve do 10
 
 v= 64
 while(v>30){
@@ -173,8 +173,8 @@ while(x<=10){
   print(paste("Broj je", x))
   x= x+1
 } # "Broj je 1"
-  # "Broj je 2"
-  # "Broj je 3"
+# "Broj je 2"
+# "Broj je 3"
 
 i= 9
 while(i>0){
@@ -193,78 +193,78 @@ while(novac>140){
   }
   novac= novac - 40
 } # "Imate 190 kn. Možete puno trošiti!"
-  # "Imate 150 kn. Možete puno trošiti!"
+# "Imate 150 kn. Možete puno trošiti!"
 
 imena= c("Josip", "Juraj", "Pepo", "Štef")
 for(i in imena){ # ovo znači da želimo ispisati svako ime u vektoru "imena"
   print(i)
 } # [1] "Josip"
-  # [1] "Juraj"
-  # [1] "Pepo"
-  # [1] "Štef
+# [1] "Juraj"
+# [1] "Pepo"
+# [1] "Štef
 
 for(i in 1:length(imena)){
   print(imena[i])
 } # [1] "Josip"
-  # [1] "Juraj"
-  # [1] "Pepo"
-  # [1] "Štef"
+# [1] "Juraj"
+# [1] "Pepo"
+# [1] "Štef"
 
 imena.list= list("Josip", "Juraj", "Pepo", "Štef")
 for(i in imena.list){ # for petlja može ići i na liste!
   print(i)
 } # [1] "Josip" 
-  # [1] "Juraj"
-  # [1] "Pepo"
-  # [1] "Štef
+# [1] "Juraj"
+# [1] "Pepo"
+# [1] "Štef
 
 for(i in 1:length(imena)){
   print(paste(imena[i], "je na poziciji", i, "u vektoru"))
 } # "Josip je na poziciji 1 u vektoru"
-  # "Juraj je na poziciji 2 u vektoru"
-  # "Pepo je na poziciji 3 u vektoru"
-  # "Štef je na poziciji 4 u vektoru"
+# "Juraj je na poziciji 2 u vektoru"
+# "Pepo je na poziciji 3 u vektoru"
+# "Štef je na poziciji 4 u vektoru"
 
 primarni= seq(1, 10, by= 2)
 for (i in primarni){
   print(i)
 } # [1] 1
-  # [1] 3
-  # [1] 5
-  # [1] 7
-  # [1] 9
+# [1] 3
+# [1] 5
+# [1] 7
+# [1] 9
 
 for (i in 1:length(primarni)){
   print(primarni[i])
 } # [1] 1
-  # [1] 3
-  # [1] 5
-  # [1] 7
-  # [1] 9
+# [1] 3
+# [1] 5
+# [1] 7
+# [1] 9
 
 kocka= c(150, -40, 30, -110, 250)
 for(i in kocka){
   print(i)
 } # [1] 150
-  # [1] -40
-  # [1] 30
-  # [1] -110
-  # [1] 250
+# [1] -40
+# [1] 30
+# [1] -110
+# [1] 250
 
 for(i in 1:length(kocka)){
   print(kocka[i])
 } # [1] 150
-  # [1] -40
-  # [1] 30
-  # [1] -110
-  # [1] 250
+# [1] -40
+# [1] 30
+# [1] -110
+# [1] 250
 
 ivan.list= list(ime= "Ivan", hobi= c("Nogomet", "Tenis", "Košarka", jedina= F))
 for(i in ivan.list){
   print(i)
 } # [1] "Ivan"
-  #                                 jedina 
-  # "Nogomet"   "Tenis" "Košarka"   "FALSE
+#                                 jedina 
+# "Nogomet"   "Tenis" "Košarka"   "FALSE
 
 for(i in 1:length(ivan.list)){
   print(ivan.list[i])
@@ -280,20 +280,20 @@ for(i in 1:nrow(matrica)){
     print(paste("U retku", i, "i stupcu", j, "matrica ima vrijednost", matrica[i,j]))
   }
 } # [1] "U retku 1 i stupcu 1 matrica ima vrijednost 1"
-  # [1] "U retku 1 i stupcu 2 matrica ima vrijednost 2"
-  # [1] "U retku 1 i stupcu 3 matrica ima vrijednost 3"
-  # [1] "U retku 2 i stupcu 1 matrica ima vrijednost 4"
-  # [1] "U retku 2 i stupcu 2 matrica ima vrijednost 5"
-  # [1] "U retku 2 i stupcu 3 matrica ima vrijednost 6"
-  # [1] "U retku 3 i stupcu 1 matrica ima vrijednost 7"
-  # [1] "U retku 3 i stupcu 2 matrica ima vrijednost 8"
-  # [1] "U retku 3 i stupcu 3 matrica ima vrijednost 9"
-  # [1] "U retku 4 i stupcu 1 matrica ima vrijednost 10"
-  # [1] "U retku 4 i stupcu 2 matrica ima vrijednost 11"
-  # [1] "U retku 4 i stupcu 3 matrica ima vrijednost 12"
-  # [1] "U retku 5 i stupcu 1 matrica ima vrijednost 13"
-  # [1] "U retku 5 i stupcu 2 matrica ima vrijednost 14"
-  # [1] "U retku 5 i stupcu 3 matrica ima vrijednost 15"
+# [1] "U retku 1 i stupcu 2 matrica ima vrijednost 2"
+# [1] "U retku 1 i stupcu 3 matrica ima vrijednost 3"
+# [1] "U retku 2 i stupcu 1 matrica ima vrijednost 4"
+# [1] "U retku 2 i stupcu 2 matrica ima vrijednost 5"
+# [1] "U retku 2 i stupcu 3 matrica ima vrijednost 6"
+# [1] "U retku 3 i stupcu 1 matrica ima vrijednost 7"
+# [1] "U retku 3 i stupcu 2 matrica ima vrijednost 8"
+# [1] "U retku 3 i stupcu 3 matrica ima vrijednost 9"
+# [1] "U retku 4 i stupcu 1 matrica ima vrijednost 10"
+# [1] "U retku 4 i stupcu 2 matrica ima vrijednost 11"
+# [1] "U retku 4 i stupcu 3 matrica ima vrijednost 12"
+# [1] "U retku 5 i stupcu 1 matrica ima vrijednost 13"
+# [1] "U retku 5 i stupcu 2 matrica ima vrijednost 14"
+# [1] "U retku 5 i stupcu 3 matrica ima vrijednost 15"
 
 pero= c(150, -40, 30, -110, 250)
 for(i in pero){
@@ -302,10 +302,10 @@ for(i in pero){
   } else
     print("Izgubio je taj dan")
 } # [1] "Pobjedio je taj dan"
-  # [1] "Izgubio je taj dan"
-  # [1] "Pobjedio je taj dan"
-  # [1] "Izgubio je taj dan"
-  # [1] "Pobjedio je taj dan"
+# [1] "Izgubio je taj dan"
+# [1] "Pobjedio je taj dan"
+# [1] "Izgubio je taj dan"
+# [1] "Pobjedio je taj dan"
 
 for(i in pero){
   print(paste("Današnji je rezultat vašeg kockanja", i))
@@ -317,8 +317,8 @@ for(i in pero){
     break # da prestane sa iteriranjem u vektoru ako dođe do toga slučaja gdje je i<100
   }
 } # [1] "Današnji je rezultat vašeg kockanja 150"
-  # [1] "Današnji je rezultat vašeg kockanja -40"
-  # [1] "Grozno kockaš, odoh!"
+# [1] "Današnji je rezultat vašeg kockanja -40"
+# [1] "Grozno kockaš, odoh!"
 
 imena= c("Josip", "Juraj", "Pepo", "Štef", "Petar Krešimir")
 for(i in imena){
@@ -327,7 +327,7 @@ for(i in imena){
   } 
   print(i)
 } # [1] "Josip" 
-  # [1] "Juraj"
+# [1] "Juraj"
 
 rec= "Ana je danas radila do 10 navečer."
 znakovi= strsplit(rec, split= "")[[1]]
@@ -341,3 +341,165 @@ for(i in 1:length(znakovi)){
 } 
 print(brojac) # Proučiti ovaj zadatak; rješavati takve zadatke.
 
+
+### Pisanje vlastitih funkcija
+
+poz= function(){
+  print("Dobar dan svima!")
+}
+poz() # "Dobar dan svima!"
+
+triple= function(x){
+  return(3*x) # ovo je eksplicitno; može se napisati samo "3*x"
+}
+triple(3) # 9
+
+tripley= function(x){
+  y= 3*x
+  return(y)
+}
+tripley(3) # 9
+
+FtoC= function(fahrenheit){
+  celsius= (fahrenheit-32)*5/9
+  return(celsius)
+}
+FtoC(10) # -12.22222
+
+square= function(x){
+  x^2
+}
+square(3) # 9 
+
+square2= function(x){
+  y= x^2
+  return(y)
+}
+square(3) # 9
+
+sumaps= function(x, y){
+  return(sum(abs(x+y)))
+}
+sumaps(-2, -3) # 5
+
+sumaps2= function(x, y= 100){
+  return(sum(abs(x+y)))
+}
+sumaps2(-800) # 700
+
+calc= function(x, y){
+  return((x/y)+1)
+}
+calc(2,5) # 1.4
+
+calc2= function(x, y= 9){
+  return((x/y)+1)
+}
+calc2(8) # 1.888889
+
+calc2= function(x, y){
+  if(y==0){
+    print("Pazite! Dijelite s 0!")
+    return(0) # Ovo da bude samo da ne piše "Inf", nego 0
+  }
+  return((x/y)+1)
+}
+calc2(7,0) # [1] "Pazite! Dijelite s 0!"
+           # [1] Inf
+
+triple= function(x, print_info= T){
+  y= x*3
+  if(print_info== T){
+    print(paste(x, "množeno sa 3 iznosi", y))
+  }
+  return(y)
+}
+triple(3) # "3 množeno sa 3 iznosi 9"
+          # 9
+
+sumax= function(x){
+  sum= 0
+  for(i in 1:length(x)){
+    sum= sum+abs(x[i])
+  }
+  sum
+}
+
+x= sample(1:10, size= 30, replace= T)
+sumax(x) # 176
+
+positive= function(x){
+  if(x>0){
+    return(x)
+  } else{
+    return(NULL)
+  }
+}
+
+square= function(x, print_this= T){
+  y= x^2
+  if(print_this== T){
+    print(paste(x, "na kvadrat je", y))
+  }
+  return(y)
+}
+square(4, FALSE) # 16
+square(4, T) # "4 na kvadrat je 16"
+             # 16
+
+kockanje= function(x){
+  if(x>0){
+    print("Bravo, pobjedili ste!")
+    return(x)
+  } else{
+    print("Loše igrate")
+    return(0)
+  }
+}
+kockanje(9) # "Bravo, pobjedili ste!"
+            # 9
+kockanje(-8) # "Loše igrate"
+             # 0
+
+kockanje.sve= function(kocka.rez, log= TRUE){
+  suma= 0
+  for(i in 1:length(x)){
+    suma= suma + kockanje(x[i])
+  }
+  if(log== T){
+    return(suma)
+  } else {
+    return(NULL)
+  }
+}
+kockanje.sve(x, T) # [1] "Bravo, pobjedili ste!"
+                   # [1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] "Bravo, pobjedili ste!"
+                   #[1] 185
